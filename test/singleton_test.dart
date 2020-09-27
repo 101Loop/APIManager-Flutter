82a26af786283d27023df5ebe28e95be9ce74528
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2020-09-27 19:38:29 +0530

    adds test case for checking single instance is created

---
+++ test/singleton_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_api_manager/src/api_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('check only single instance is created', () {
+    /// Create an instance of [APIManager]
+    APIManager instance = APIManager.getInstance(baseUrl: 'google');
+
+    /// Create another instance of [APIManager]
+    APIManager instance1 = APIManager.getInstance();
+
+    /// Check if the same instance is returned or not
+    expect(instance.baseUrl, 'google');
+    expect(instance1.baseUrl, 'google');
+  });
+}
