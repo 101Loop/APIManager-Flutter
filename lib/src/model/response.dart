839704cd92603c06584af5f4401d34eef2b70c41
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2020-10-02 10:49:55 +0530

    adds response model

---
+++ lib/src/model/response.dart
@@ -0,0 +1,17 @@
+/// Response class
+/// Will be used for returning the response from API call
+class Response {
+  final data;
+  final int statusCode;
+  final String error;
+  final rawData;
+  final bool isSuccessful;
+
+  Response({
+    this.data,
+    this.statusCode = -1,   // -1 will be returned if the response couldn't be processed somehow
+    this.error,
+    this.rawData,
+    this.isSuccessful,
+  });
+}
