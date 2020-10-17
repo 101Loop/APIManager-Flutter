676178a1ef7fcaa752a55cd7d3ae379abff93af9
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2020-10-17 22:32:54 +0530

    adds exception class

---
+++ lib/src/exception/exception.dart
@@ -0,0 +1,10 @@
+/// Custom exception class
+class APIException implements Exception {
+  /// Error in string format
+  String error;
+
+  /// Status code of the API call
+  int statusCode;
+
+  APIException(this.error, {this.statusCode = 1});
+}
