7de6aa625f059e61527f979457b8af7b3720acb2
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2022-01-12 00:41:31 +0530

    removes unnecessary import from test

--- test/api_call_test.dart
+++ test/api_call_test.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_api_manager/flutter_api_manager.dart';
-import 'package:flutter_api_manager/src/exception/exception.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
